39716a780e8a78f31a08952766e5c7428a61b304
Getho <Getho@TT>
2018-12-24 18:31:07 +0000

    Async process creation.

--- Pandora/Misc/ProcessCreator.cs
+++ Pandora/Misc/ProcessCreator.cs
@@ -1,12 +1,117 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Pandora.ProcessCreation
-{
+{ 
     public sealed class ProcessCreator
     {
-        public static bool Create(string pathToProcess, ProcessCreationFlags processCreationFlags, out Process process, out PROCESS_INFORMATION processInformation)
+        public readonly string PathToProcess;
+        public readonly WindowState WindowState;
+        public readonly StartUpInfo StartUpInfo;
+
+
+        public ProcessCreator(string pathToProcess, WindowState windowState = WindowState.SW_SHOWNORMAL, StartUpInfo? startUpInfo = null)
+        {
+            PathToProcess = pathToProcess;
+            WindowState = windowState;
+            StartUpInfo = startUpInfo ?? new StartUpInfo() {
+                dwFlags = 0x00000001,
+                wShowWindow = (short)WindowState
+            };
+        }
+
+
+        public async Task<CreateProcessData> CreateTaskAsync()
+        {
+            CreateProcessData createProcessData = new CreateProcessData();
+
+            if (!System.IO.File.Exists(PathToProcess))
+                return createProcessData;
+
+            StartUpInfo startUpInfo = StartUpInfo;
+            ProcessInformation processInformation;
+            SecurityAttributes threadAttributes = new SecurityAttributes();
+            SecurityAttributes processAttributes = new SecurityAttributes();
+
+            var processCreated =
+                CreateProcess(
+                    PathToProcess,
+                    null,
+                    ref processAttributes, ref threadAttributes,
+                    false,
+                    ProcessCreationFlags.CREATE_DEFAULT_ERROR_MODE,
+                    IntPtr.Zero,
+                    null,
+                    ref startUpInfo, out processInformation);
+
+            if (processCreated) {
+
+                createProcessData.Process = Process.GetProcessById(processInformation.dwProcessId);
+                createProcessData.ProcessInformation = processInformation;
+
+                return await Task.Run(() => {
+
+                    // 5 second timeout...
+
+                    for (
+                        int cTick = Environment.TickCount, eTick = (cTick + 5_000);
+                        cTick < eTick || !createProcessData.WindowLoaded;
+                        cTick++) {
+
+                        createProcessData.Process.Refresh();
+
+                        if (!string.IsNullOrWhiteSpace(createProcessData.Process.MainWindowTitle)) {
+                            createProcessData.WindowLoaded = true;
+                        }
+                    }
+
+                    return createProcessData;
+                });
+            }
+
+            return createProcessData;
+        }
+        
+
+        public static async Task<bool> CreateTaskAsync(string pathToProcess)
+        {
+            if (!System.IO.File.Exists(pathToProcess))
+                return false;
+
+            StartUpInfo startUpInfo = new StartUpInfo() {
+                dwFlags = 0x00000001,
+                wShowWindow = 7
+            };
+
+            ProcessInformation processInformation;
+            SecurityAttributes threadAttributes = new SecurityAttributes();
+            SecurityAttributes processAttributes = new SecurityAttributes();
+
+            var processCreated =
+                CreateProcess(pathToProcess, null, ref processAttributes, ref threadAttributes, false, ProcessCreationFlags.CREATE_DEFAULT_ERROR_MODE, IntPtr.Zero, null, ref startUpInfo, out processInformation);
+
+            if (processCreated) {
+
+                Process process = Process.GetProcessById(processInformation.dwProcessId);
+
+                return await Task.Run(() => {
+                    // Sometimes, when creating a new process, the MainWindowHandle property is zero until the two while loops finish...
+                    //
+                    while (!process.WaitForInputIdle(100)) {
+                    }
+                    while (string.IsNullOrWhiteSpace(process.MainWindowTitle)) {
+                    }
+                    return true;
+                });
+            }
+
+            return processCreated;
+        }
+
+        public static bool Create(string pathToProcess, ProcessCreationFlags processCreationFlags, out Process process, out ProcessInformation processInformation)
         {
             process = null;
             processInformation = default;
@@ -14,43 +119,67 @@ namespace Pandora.ProcessCreation
             if (!System.IO.File.Exists(pathToProcess))
                 return false;
 
-            STARTUPINFO startUpInfo = new STARTUPINFO();
-            SECURITY_ATTRIBUTES threadAttributes = new SECURITY_ATTRIBUTES();
-            SECURITY_ATTRIBUTES processAttributes = new SECURITY_ATTRIBUTES();
+            StartUpInfo startUpInfo = new StartUpInfo();
+            SecurityAttributes threadAttributes = new SecurityAttributes();
+            SecurityAttributes processAttributes = new SecurityAttributes();
 
-            var processCreated = 
+            var processCreated =
                 CreateProcess(pathToProcess, null, ref processAttributes, ref threadAttributes, false, processCreationFlags, IntPtr.Zero, null, ref startUpInfo, out processInformation);
 
-            if (processCreated) {
+            if (processCreated)
                 process = Process.GetProcessById(processInformation.dwProcessId);
-            }
-      
+
             return processCreated;
         }
 
-        public static bool Create(string pathToProcess, out Process process, out PROCESS_INFORMATION processInformation) =>
+        public static bool CreateDefault(string pathToProcess, out Process process, out ProcessInformation processInformation) =>
             Create(pathToProcess, ProcessCreationFlags.CREATE_DEFAULT_ERROR_MODE, out process, out processInformation);
 
-        public static bool CreateSuspended(string pathToProcess, out Process process, out PROCESS_INFORMATION processInformation) =>
+        public static bool CreateSuspended(string pathToProcess, out Process process, out ProcessInformation processInformation) =>
             Create(pathToProcess, ProcessCreationFlags.CREATE_SUSPENDED, out process, out processInformation);
 
 
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         private static extern bool CreateProcess(
             string lpApplicationName, string lpCommandLine, 
-            ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes,
+            ref SecurityAttributes lpProcessAttributes, ref SecurityAttributes lpThreadAttributes,
             bool bInheritHandles, 
             ProcessCreationFlags dwCreationFlags, 
             IntPtr lpEnvironment, 
             string lpCurrentDirectory, 
-            [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+            [In] ref StartUpInfo lpStartupInfo, out ProcessInformation lpProcessInformation);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern uint ResumeThread(IntPtr hThread);
     }
 
+    public enum WindowState : short
+    {
+        SW_HIDE = 0,
+        SW_SHOWNORMAL = 1,
+        SW_SHOWMINIMIZED = 2,
+        SW_MAXIMIZE = 3,
+        SW_MINIMIZE = 6,
+        SW_SHOWMINNOACTIVE = 7,
+        SW_FORCEMINIMIZE = 11
+    }
+
+    public struct CreateProcessData
+    {
+        public bool WindowLoaded;
+        public Process Process;
+        public ProcessInformation ProcessInformation;
+
+        //public CreateProcessData(bool result, Process process, ProcessInformation processInformation)
+        //{
+        //    Result = result;
+        //    Process = process;
+        //    ProcessInformation = processInformation;
+        //}
+    }
+
     [StructLayout(LayoutKind.Sequential)]
-    public struct STARTUPINFO
+    public struct StartUpInfo
     {
         public uint cb;
         public string lpReserved;
@@ -73,7 +202,7 @@ namespace Pandora.ProcessCreation
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct PROCESS_INFORMATION
+    public struct ProcessInformation
     {
         public IntPtr hProcess;
         public IntPtr hThread;
@@ -82,7 +211,7 @@ namespace Pandora.ProcessCreation
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct SECURITY_ATTRIBUTES
+    public struct SecurityAttributes
     {
         public int nLength;
         public IntPtr lpSecurityDescriptor;
